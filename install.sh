0e5e131d7eb7958957e6cf3d49ef2d0ce1014930
Griffin Sectio <grifin.dwi13@gmail.com>
2024-08-02 22:02:09 +0700

    completing download process

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+if [ ! $EUID -eq 0 ]
+then
+	echo "Please run this script with root privilege"
+	exit
+fi
+
 pacman -Sy python3 ffmpeg
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
 mv ./yt-dlp /usr/bin/
 chmod +x /usr/bin/yt-dlp
+make
+mv build/ytdlpw /usr/bin/
--- src/main.c
+++ src/main.c
@@ -1,19 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
-void itoa(int num) {
-    char res = (char) num;
-    printf("%d\n", res+10);
-    printf("%d\n", res);
+const char *ytdlp = "yt-dlp";
+const char *outputFormat = "-o \"%(title)s.%(ext)s\" --restrict-filenames";
+
+void download(char *url, char *format) {
+    int commandLen = strlen(ytdlp) + strlen(outputFormat) + strlen(format) + strlen(url) + 1;
+    char *command = (char *) malloc(commandLen);
+    sprintf(command, "%s %s %s %s", ytdlp, outputFormat, format, url);
+    command[commandLen-1] = '\0';
+
+    int exit_code = system(command);
+
+    if (WEXITSTATUS(exit_code) != 0) {
+        printf("Couldn't download the file with specified format/quality\n");
+        exit(1);
+        // download(url, format);
+    }
 }
 
-void download() {
+void downloadVideoAudio(char *url) {
+    char quality[9][5] = {"144", "240", "360", "480", "720", "1080", "1440", "2160", "4320"};
+    unsigned long len = sizeof(quality)/sizeof(char*);
+    
+    for (unsigned long i = 0; i < len; i++) {
+        printf("%lu. %sp\n", i+1, quality[i]);
+    }
+
+    unsigned long userChoice;
+    int res = scanf("%lu", &userChoice);
+    if (res != 1) {
+        printf("Please enter a valid number!\n");
+    }
+
+    char *resolution = (char *)malloc(6 * sizeof(char));
+
+    if (userChoice > 0 && userChoice < len + 1) {
+        resolution = quality[userChoice-1];
+    } else {
+        printf("Please select one of available resolution!\n");
+        exit(1);
+    }
+
+    char formatString[] = "-f \"bv*[height<=%s]+ba\"";
+    char *format = (char *) malloc(strlen(resolution) + strlen(formatString));
+    sprintf(format, formatString, resolution);
+    
+    download(url, format);
 
 }
 
-void fileToDownload() {
-    char *options[] = {
+void downloadVideo(char *url) {
+    char quality[9][5] = {"144", "240", "360", "480", "720", "1080", "1440", "2160", "4320"};
+    unsigned long len = sizeof(quality)/sizeof(char*);
+    
+    for (unsigned long i = 0; i < len; i++) {
+        printf("%lu. %sp\n", i+1, quality[i]);
+    }
+
+    unsigned long userChoice;
+    int res = scanf("%lu", &userChoice);
+    if (res != 1) {
+        printf("Please enter a valid number!\n");
+    }
+
+    char *resolution = (char *)malloc(6 * sizeof(char));
+
+    if (userChoice > 0 && userChoice < len + 1) {
+        resolution = quality[userChoice-1];
+    } else {
+        printf("Please select one of available resolution!\n");
+        exit(1);
+    }
+
+    char formatString[] = "-f \"bv*[height=%s]\"";
+    char *format = (char *) malloc(strlen(resolution) + strlen(formatString));
+    sprintf(format, formatString, resolution);
+    
+    download(url, format);
+}
+
+void downloadAudio(char * url) {
+    char format[] = "-f \"ba\"";
+    download(url, format);
+}
+
+void mainMenu(char * url) {
+    char options[3][12] = {
         "video+audio",
         "video only",
         "audio only"
@@ -32,34 +107,34 @@ void fileToDownload() {
     if  (result != 1) {
         printf("Please enter a valid number!\n");
         return;
-    } else if (userChoice <= 0 || userChoice > len) {
-        printf("Please select one of the option!\n");
-        return;
     }
 
+    switch (userChoice)
+    {
+    case 1:
+        downloadVideoAudio(url);
+        break;
+    case 2:
+        downloadVideo(url);
+        break;
+    case 3: 
+        downloadAudio(url);
+        break;
+    default:
+        printf("Please select one of the option!\n");
+        break;
+    }
     printf("\n");
-    printf("%d\n", result);
-
-    printf("%d\n", len);
 }
 
 int main(int argc, char **argv) {
-    itoa(10);
-    return 0;
-    fileToDownload();
-
     if (argc != 2) {
-        printf("USAGE: \n");
-        printf("%s URL\n", argv[0]);
+        printf("USAGE:\n%s URL\n", argv[0]);
         return 1;
     }
 
-    char command[] = "yt-dlp --list-formats ";
     char *url = argv[1];
+    mainMenu(url);
 
-    strcat(command, url);
-
-    // printf("Hello, world!\n");
-    system(command);
     return 0;
 }
